16249ca0916d1ce460955d1439522d12336302c8
Chris <lucianichristophe0@gmail.com>
2021-07-20 11:58:33 +0200

    Part 03 Ok

---
+++ app/Http/Helpers/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+function makeShortCutName($name)
+{
+    $name_part = explode(" ", $name);
+
+    /* 1ère lettre du Nom et Prénom */
+    $letter_1 = substr($name_part[0], 0, 1);
+    $letter_2 = substr($name_part[1], 0, 1);
+
+    $name = $letter_1 . $letter_2;
+
+    return $name;
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row chat-row">
-    <div class="col-md-3">
-        <div class="users">
-            <h5>Users</h5>
+    <div class="row chat-row">
+        <div class="col-md-3">
+            <div class="users">
 
-            <ul class="list-group list-chat-item">
-                <li class="chat-user-list">
-                    <a href="">
-                        Manohar
-                    </a>
-                </li>
-            </ul>
+                <h5>Users</h5>
+
+                <ul class="list-group list-chat-item">
+                    @if ($users->count())
+                        @foreach ($users as $user)
+                            <li class="chat-user-list">
+                                <a href="" class="d-flex align-items-center">
+                                    <div class="chat-image  bg-primary">
+                                        <div class="name-image">
+                                            {{ makeShortCutName($user->name) }}
+                                        </div>
+                                    </div>
+                                    <div class="chat-name ml-1">
+                                        {{ $user->name }}
+                                    </div>
+                                </a>
+                            </li>
+                        @endforeach
+                    @endif
+                </ul>
+            </div>
         </div>
+
+        <div class="col-md-9">
+            <h1>
+                Message Section
+            </h1>
+            Select user from the list to begin conversation.
+        </div>
+
     </div>
-
-    <div class="col-md-9">
-
-    </div>
-
-</div>
 
 @endsection
