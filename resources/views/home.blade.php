e1efb61fafb5e892d4d2224e7f71d1eaad9d6f0e
Chris <lucianichristophe0@gmail.com>
2021-08-02 15:13:00 +0200

    https ERR SSL PROTOCAL ERROR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,7 +93,7 @@
             $('.js-example-basic-single').select2();
 
             let user_id = "{{ auth()->user()->id }}"
-            let ip_address = '51.77.157.244';
+            let ip_address = '127.0.0.1';
             let socket_port = '3000';
             let socket = io(ip_address + ':' + socket_port)
 
