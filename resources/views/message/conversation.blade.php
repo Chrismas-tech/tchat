a9951b695d6749a71ed45b785f93392e4a3c9f98
Chris <lucianichristophe0@gmail.com>
2021-08-05 18:19:25 +0200

    set timeout writing

--- resources/views/message/conversation.blade.php
+++ resources/views/message/conversation.blade.php
@@ -438,7 +438,7 @@
                     /* AUCUNE IDEE MAIS SANS UN SET-TIMEOUT CA NE MARCHE PAS !!!! */
                     setTimeout(() => {
                         document.getElementById(attribute).remove()
-                    }, 1);
+                    }, 100);
                 }
 
             })
