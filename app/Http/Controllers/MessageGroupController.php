e18c501a8181b1796434f18ba0d37bd3fabc6d7f
Openclassroom Luciani <lucianichristophe0@gmail.com>
2021-08-19 12:45:51 +0200

    style result ajax

--- app/Http/Controllers/MessageGroupController.php
+++ app/Http/Controllers/MessageGroupController.php
@@ -6,35 +6,15 @@ use App\Models\Message;
 use App\Models\MessageGroup;
 use App\Models\MessageGroupMember;
 use App\Models\User;
+use App\Models\UserMessage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MessageGroupController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth');
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -85,46 +65,12 @@ class MessageGroupController extends Controller
         $groups = MessageGroup::all();
         $currentGroup = MessageGroup::where('id', '=', $groupId)->first();
 
-        $members_of_group = MessageGroupMember::where('message_group_id', '=', $groupId)->get();
-        $messages_of_group = Message::all();
+        $members_of_this_group = MessageGroupMember::where('message_group_id', '=', $groupId)->get();
+        $messages_of_this_group = UserMessage::where('message_group_id', '=', $groupId)->get();
 
-        /* ->with('message_group_members.user')
-        ->first(); */
+        /* dd($images_members); */
+        /* dd($members_of_group); */
 
-        return view('message_groups.group', compact('users', 'user', 'members_of_group', 'user_full_name', 'groups', 'currentGroup', 'messages_of_group'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('message_groups.group', compact('users', 'user', 'user_full_name', 'groups', 'currentGroup', 'messages_of_this_group', 'members_of_this_group'));
     }
 }
